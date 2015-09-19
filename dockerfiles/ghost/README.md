18df83e274fecf8d7ca7d998464281a9918f59d5
Jordon Bedwell <jordon@envygeeks.io>
2015-09-18 20:05:27 -0500

    Add a Ghost image, people asked me to build one.

---
+++ dockerfiles/ghost/README.md
@@ -0,0 +1,30 @@
+# Ghost Docker Image
+
+* [![](https://badge.imagelayers.io/envygeeks/ghost:latest.svg)][latest] `latest`
+[latest]: https://imagelayers.io?images=envygeeks/ghost:latest
+
+## Usage
+
+```
+docker run --rm -p 4000:4000 \
+  -dit envygeeks/ghost
+```
+
+## Environment variables
+
+* `$PORT` - Default: `4000`.
+* `$DOMAIN` - Default: `localhost`.
+* `$LISTEN_ADDRESS` - Default: `0.0.0.0`.
+* `$MAIL_SERVICE` - Mandrill, MailGun, SMTP.
+* `$MAIL_USER` - Username.
+* `$MAIL_PASS` - API Key or Password.
+* `$DATABASE_TYPE` - PostgreSQL, MySQL - Default: `pg`
+* `$DATABASE_HOST` - DB hostname - Default: `postgresql`.
+* `$DATABASE_PORT` - DB port - Default: `5432`.
+* `$DATABASE_USER` - DB user - Default: `ghost`.
+* `$DATABASE` - The DB - Default: `ghost`.
+* `$DATABASE_PASS` - DB pass.
+
+## Database / Storage
+
+By default this image will use SQLite and store the database in `/srv/ghost` for you, so that you can mount a data container or preferably a local folder and persist the data, if you wish to use an external database use environment variables to set the data for the configuration.
---
+++ dockerfiles/ghost/copy/usr/local/share/ghost/config.js
@@ -0,0 +1,58 @@
+var   path = require("path");
+var config = {
+  production: {
+    url: "http://" + (process.env.DOMAIN || "localhost") + ":" + (process.env.PORT || "4000"),
+    smtp: {},
+
+    paths: {
+      contentPath: "/srv/ghost"
+    },
+
+    database: {
+      client: "sqlite3",
+      debug: false,
+      connection: {
+        filename: "/srv/ghost/data/ghost.db"
+      }
+    },
+
+    server: {
+      port: process.env.PORT || 4000,
+      host: process.env.LISTEN_ADDRESS || "0.0.0.0"
+    }
+  }
+};
+
+if (process.env.SMTP_SERVICE) {
+  config.production.smtp = {
+    mail: {
+      transport: "SMTP",
+      options: {
+        service: process.env.SMTP_SERVICE || "Mandrill", auth: {
+           user: process.env.SMTP_USER,
+           pass: process.env.SMTP_PASS
+        }
+      }
+    }
+  };
+}
+
+if (process.env.DATABASE_TYPE && process.env.DATABASE_TYPE.toLowerCase() != "sqlite") {
+  config.production.database = {
+    client: process.env.DATABASE_TYPE || "pg",
+    debug: false,
+    connection: {
+      user: process.env.DATABASE_USER || "ghost",
+      database: process.env.DATABASE  || "ghost",
+      charset: process.env.DATABASE_CHARSET || "utf8",
+      host: process.env.DATABASE_HOST || "postgresql",
+      port: process.env.DATABASE_PORT || 5432,
+      password: process.env.DATABASE_PASS
+    }
+  };
+}
+
+module.exports = config;
+console.log(JSON.stringify(
+  config, null, 2
+));
