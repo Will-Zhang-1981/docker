7375f57fe95823a31a07962994112b5ce9441c4b
Jordon Bedwell <jordon@envygeeks.io>
2015-10-08 22:11:42 -0500

    Reorganize and support TryGhost (Ghost) in development too, for themes.

--- dockerfiles/ghost/copy/usr/local/lib/ghost/config.js
+++ dockerfiles/ghost/copy/usr/local/lib/ghost/config.js
@@ -1,38 +1,53 @@
-var     path = require("path"),
-  identifier = process.env.FORCE_SSL ? "https://" : "http://";
-        port = process.env.USE_PROXY ? "" : (":" +
-          process.env.PORT || "4000");
+if (process.env.GHOST_ENV && 0 > ["development", "production"].indexOf(process.env.GHOST_ENV)) {
+  throw "You have not given a valid GHOST_ENV.";
+}
+
+// ----------------------------------------------------------------------------
+
+var path = require("path");
+var identifier = process.env.FORCE_SSL ? "https://" : "https://";
+var database = { client: "sqlite3", debug: false, connection: { filename: "/srv/ghost/data/ghost.db" }};
+var server = { port: process.env.PORT || 4000, host: process.env.LISTEN_ADDRESS || "0.0.0.0" };
+var port = process.env.USE_PROXY ? "" : (":" + (process.env.PORT || "4000"));
+var privacy = { useUpdateCheck: false, useGoogleFonts: false };
+var paths = { contentPath: "/srv/ghost" };
+
+// ----------------------------------------------------------------------------
 
 var config = {
   production: {
+    paths: paths,
+    privacy: privacy,
     url: process.env.URL || (identifier + (process.env.DOMAIN || "localhost") + port),
+    database: database,
+    server: server,
     smtp: {},
+  },
 
-    paths: {
-      contentPath: "/srv/ghost"
-    },
-
-    database: {
-      client: "sqlite3",
-      debug: false,
-      connection: {
-        filename: "/srv/ghost/data/ghost.db"
-      }
-    },
-
-    server: {
-      port: process.env.PORT || 4000,
-      host: process.env.LISTEN_ADDRESS || "0.0.0.0"
-    }
+  development: {
+    paths: paths,
+    privacy: privacy,
+    url: "http://localhost" + port,
+    database: database,
+    server: server,
+    smtp: {},
   }
 };
 
+// ----------------------------------------------------------------------------
+// We actually default to smtp: {} which will try and connect to localhost.
+// Ghost will nag you though about an explict configuration for some reason...
+// even if it can connect to localhost, and that is annoying.
+// ----------------------------------------------------------------------------
+
 if (process.env.SMTP_SERVICE) {
+  config[process.env.GHOST_ENV || "production"].smtp =
   config.production.smtp = {
     mail: {
       transport: "SMTP",
       options: {
-        service: process.env.SMTP_SERVICE || "Mandrill", auth: {
+        service: process.env.SMTP_SERVICE || "Mandrill",
+        auth: {
            user: process.env.SMTP_USER,
            pass: process.env.SMTP_PASS
         }
@@ -41,8 +56,10 @@ if (process.env.SMTP_SERVICE) {
   };
 }
 
+// ----------------------------------------------------------------------------
+
 if (process.env.DATABASE_TYPE && process.env.DATABASE_TYPE.toLowerCase() != "sqlite") {
-  config.production.database = {
+  config[process.env.GHOST_ENV || "production"].database = {
     client: process.env.DATABASE_TYPE || "pg",
     debug: false,
     connection: {
@@ -56,5 +73,9 @@ if (process.env.DATABASE_TYPE && process.env.DATABASE_TYPE.toLowerCase() != "sql
   };
 }
 
+// ----------------------------------------------------------------------------
+
 module.exports = config;
-console.log(JSON.stringify(config, null, 2))
+console.log(JSON.stringify(
+  config, null, 2
+));
